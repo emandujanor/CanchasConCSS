3dd8124b65ab85dba183d2fd01508188305f317a
Edith Mandujano <edithmandujano001@gmail.com>
2017-04-10 23:23:14 -0500

    Con cancha de Futbol

--- js/canchas.js
+++ js/canchas.js
@@ -1,19 +1,26 @@
 //alert("aqui toy");
 var botonFutbol= document.getElementById('futbol');
 var botonBasket= document.getElementById('basket');
+var botonTenis= document.getElementById('tenis');
 var canchas=document.getElementsByClassName('fondo');
 botonFutbol.addEventListener("click", mostrarFutbol);
 botonBasket.addEventListener("click", mostrarBascket);
+botonTenis.addEventListener("click", mostrarTenis);
+
 
 function mostrarFutbol(){
-
   canchas[0].style.display="block";
   canchas[1].style.display="none";
+  canchas[2].style.display="none";
 
 }
 function mostrarBascket(){
-
   canchas[1].style.display="block";
   canchas[0].style.display="none";
-
+  canchas[2].style.display="none";
+}
+function mostrarTenis(){
+  canchas[1].style.display="none";
+  canchas[0].style.display="none";
+  canchas[2].style.display="block";
 }
